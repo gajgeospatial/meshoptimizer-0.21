2ca75d70564b2d320f34bf0f41cd9e097ed022d5
Glen Johnson <glen.johnson@simblocks.io>
2024-09-11 19:20:01 -0400

    Initial Checkin

---
+++ msvc/CMakeFiles/Export/lib/cmake/meshoptimizer/meshoptimizerTargets-minsizerel.cmake
@@ -0,0 +1,28 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "MinSizeRel".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "meshoptimizer::meshoptimizer" for configuration "MinSizeRel"
+set_property(TARGET meshoptimizer::meshoptimizer APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(meshoptimizer::meshoptimizer PROPERTIES
+  IMPORTED_IMPLIB_MINSIZEREL "${_IMPORT_PREFIX}/lib/meshoptimizer.lib"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/bin/meshoptimizer.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS meshoptimizer::meshoptimizer )
+list(APPEND _IMPORT_CHECK_FILES_FOR_meshoptimizer::meshoptimizer "${_IMPORT_PREFIX}/lib/meshoptimizer.lib" "${_IMPORT_PREFIX}/bin/meshoptimizer.dll" )
+
+# Import target "meshoptimizer::gltfpack" for configuration "MinSizeRel"
+set_property(TARGET meshoptimizer::gltfpack APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(meshoptimizer::gltfpack PROPERTIES
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/bin/gltfpack.exe"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS meshoptimizer::gltfpack )
+list(APPEND _IMPORT_CHECK_FILES_FOR_meshoptimizer::gltfpack "${_IMPORT_PREFIX}/bin/gltfpack.exe" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
---
+++ msvc/CMakeFiles/Export/lib/cmake/meshoptimizer/meshoptimizerTargets-relwithdebinfo.cmake
@@ -0,0 +1,28 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "meshoptimizer::meshoptimizer" for configuration "RelWithDebInfo"
+set_property(TARGET meshoptimizer::meshoptimizer APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(meshoptimizer::meshoptimizer PROPERTIES
+  IMPORTED_IMPLIB_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/meshoptimizer.lib"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/bin/meshoptimizer.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS meshoptimizer::meshoptimizer )
+list(APPEND _IMPORT_CHECK_FILES_FOR_meshoptimizer::meshoptimizer "${_IMPORT_PREFIX}/lib/meshoptimizer.lib" "${_IMPORT_PREFIX}/bin/meshoptimizer.dll" )
+
+# Import target "meshoptimizer::gltfpack" for configuration "RelWithDebInfo"
+set_property(TARGET meshoptimizer::gltfpack APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(meshoptimizer::gltfpack PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/bin/gltfpack.exe"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS meshoptimizer::gltfpack )
+list(APPEND _IMPORT_CHECK_FILES_FOR_meshoptimizer::gltfpack "${_IMPORT_PREFIX}/bin/gltfpack.exe" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
